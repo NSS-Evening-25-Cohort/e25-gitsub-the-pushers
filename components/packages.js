c9f98e8bf0bb95e1bfacc4c63b9a4636470c5bef
Jay Lhomme <jhgatohagire@gmail.com>
2023-11-07 19:20:22 -0600

    skeleton packages card

---
+++ components/packages.js
@@ -0,0 +1,9 @@
+export const packages = () => {
+  return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h4 class="card-title"><b>${packages.title}</b></h4>
+    <p class="card-text">${packages.description}</p>
+    <a href="#" class="btn btn-secondary">Learn More</a>
+  </div>
+</div>`
+}
