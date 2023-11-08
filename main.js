b294d793f2657054aa08456c296577e32036031e
Maria Sanli <marisanli.93@gmail.com>
2023-11-07 21:04:18 -0500

    overview card added to components

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { repos } from "./data/reference";
 import { card } from "./components/card";
 import { navbar } from "./components/navbar";
 import { footer } from "./components/footer";
+import { overviewPage } from "./components/overviewPage";
 
-// TODO: profile.js
+
 // TODO: form.js
