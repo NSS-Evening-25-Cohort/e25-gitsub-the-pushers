4700207ae3db9fd90952fcc471c7e19664f0e635
Bruce Clark <bruce.clark55@gmail.com>
2023-11-11 14:17:46 -0500

    nav bar is set at top of the page

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,31 @@
 export const navbar = () => {
-  return `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+return`
+<nav class="navbar fixed-top bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <div class="navbar-nav">
-      <a class="nav-link" id="overviewBtn" href="#">Overview</a>
-      <a class="nav-link" id="reposBtn" href="#">Repos</a>
-      <a class="nav-link" id="projectsBtn" href="#">Projects</a>
-      <a class="nav-link" id="packagesBtn" href="#">Packages</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" id="overviewBtn" aria-current="page" href="#">Overview</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" id="reposBtn" href="#">Repos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" id="projectsBtn" href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" id="packagesBtn" href="#">Packages</a>
+        </li>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
+  </nav>
 </nav>`;
-};
+}
